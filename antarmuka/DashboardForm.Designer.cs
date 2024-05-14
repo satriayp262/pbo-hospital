9d160975155dd38ee9c4037251db8ff8ec9ab86e
Pradita191d <134593226+Pradita191d@users.noreply.github.com>
2024-05-14 14:19:07 +0700

    Add files via upload

---
+++ antarmuka/DashboardForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace RumahSakit
+{
+    partial class DashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dataMasterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dokterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ruanganToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasienToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rekamMedisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dataMasterToolStripMenuItem,
+            this.logOutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dataMasterToolStripMenuItem
+            // 
+            this.dataMasterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dokterToolStripMenuItem,
+            this.ruanganToolStripMenuItem,
+            this.pasienToolStripMenuItem,
+            this.rekamMedisToolStripMenuItem});
+            this.dataMasterToolStripMenuItem.Name = "dataMasterToolStripMenuItem";
+            this.dataMasterToolStripMenuItem.Size = new System.Drawing.Size(104, 24);
+            this.dataMasterToolStripMenuItem.Text = "Data Master";
+            // 
+            // dokterToolStripMenuItem
+            // 
+            this.dokterToolStripMenuItem.Name = "dokterToolStripMenuItem";
+            this.dokterToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.dokterToolStripMenuItem.Text = "Dokter";
+            // 
+            // ruanganToolStripMenuItem
+            // 
+            this.ruanganToolStripMenuItem.Name = "ruanganToolStripMenuItem";
+            this.ruanganToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ruanganToolStripMenuItem.Text = "Ruangan";
+            this.ruanganToolStripMenuItem.Click += new System.EventHandler(this.ruanganToolStripMenuItem_Click);
+            // 
+            // pasienToolStripMenuItem
+            // 
+            this.pasienToolStripMenuItem.Name = "pasienToolStripMenuItem";
+            this.pasienToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pasienToolStripMenuItem.Text = "Pasien";
+            // 
+            // rekamMedisToolStripMenuItem
+            // 
+            this.rekamMedisToolStripMenuItem.Name = "rekamMedisToolStripMenuItem";
+            this.rekamMedisToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.rekamMedisToolStripMenuItem.Text = "Rekam Medis";
+            // 
+            // logOutToolStripMenuItem
+            // 
+            this.logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
+            this.logOutToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.logOutToolStripMenuItem.Text = "Log out";
+            // 
+            // DashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DashboardForm";
+            this.Text = "CareConnect";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dataMasterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dokterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ruanganToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasienToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rekamMedisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logOutToolStripMenuItem;
+    }
+}
+
---
+++ model/RuanganCls.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahSakit.model
+{
+    using konfigurasi;
+    using System.Data;
+
+    internal class RuanganCls
+    {
+        string kode_ruangan;
+        string nama_ruangan;
+        string status_ruangan;
+
+        LayananCls server;
+        string Query;
+        DataTable temp;
+
+
+        public RuanganCls()
+            {
+                kode_ruangan = "";
+                nama_ruangan = "";
+                status_ruangan = "";
+                server = new LayananCls();
+                temp = new DataTable();
+                Query = "";
+            }
+
+        public string setkode_ruangan
+        {
+            set { kode_ruangan = value; }
+        }
+
+
+        public string setnama_ruangan
+        {
+            set { nama_ruangan = value; }
+        }
+
+
+        public string setstatus_ruangan
+        {
+            set { status_ruangan = value; }
+        }
+
+        public bool apakahAda(string kode)
+        {
+            bool cek = false;
+            Query = "select * from kamar where no_kmr ='" + kode + "'";
+            temp = server.eksekusiQuery(Query);
+            if (temp.Rows.Count > 0)
+            {
+                cek = true;
+            }
+
+            return cek;
+        }
+
+        public int simpanData()
+        {
+            int result = -1;
+
+            Query = "insert into kamar (no_kmr, nama_kmr, status_kmr) values('" + kode_ruangan + "', " +
+                "'" + nama_ruangan + "', '" + status_ruangan + "')";
+
+            try
+            {
+                // Eksekusi kueri menggunakan objek server
+                result = server.eksekusiNonQuery(Query);
+                if (result < 0)
+                {
+                    throw new Exception("Gagal menyimpan data");
+                }
+            }
+            catch (Exception e) { }
+
+            return result;
+        }
+
+        public int ubahData(string kode)
+        {
+            int result = -1;
+
+            Query = "update kamar set nama_kmr='" + nama_ruangan + "', status_kmr='" + status_ruangan + "' where no_kmr='" + kode + "'";
+
+            try
+            {
+                result = server.eksekusiNonQuery(Query);
+                if (result < 0)
+                {
+                    throw new Exception("Gagal mengubah data");
+                }
+            }
+            catch (Exception e) { }
+
+            return result;
+        }
+
+        public int hapusData(string kode)
+        {
+            int result = -1;
+
+            Query = "delete from kamar where no_kmr='" + kode + "'";
+
+            try
+            {
+                result = server.eksekusiNonQuery(Query);
+                if (result < 0)
+                {
+                    throw new Exception("Gagal menghapus data");
+                }
+            }
+            catch (Exception e) { }
+
+            return result;
+        }
+
+        public DataTable tampilSemua()
+        {
+            Query = "select * from kamar";
+
+            return server.eksekusiQuery(Query);
+        }
+
+
+        public string buatKode()
+        {
+            string kode = "";
+            int nilai = 0;
+
+            Query = "SELECT IFNULL(MAX(no_kmr),0)+1 AS kode FROM kamar";
+
+            temp = server.eksekusiQuery(Query);
+
+            if (temp.Rows.Count > 0)
+            {
+                foreach (DataRow data in temp.Rows)
+                {
+                    nilai = Convert.ToInt32(data[0]);
+                }
+                if (nilai > 0 && nilai < 10)
+                {
+                    kode = "00" + nilai.ToString();
+                }
+                else if (nilai >= 10 && nilai < 100)
+                {
+                    kode = nilai.ToString();
+                }
+            }
+            return kode;
+        }
+
+
+        public DataTable cariDgNama(string nama)
+        {
+            Query = "select * from kamar where nama_kmr like '%" + nama + "%'";
+            return server.eksekusiQuery(Query);
+        }
+
+
+        public bool getStatusKamar(string noRuangan)
+        {
+            // Query untuk mengecek apakah kamar sudah terisi
+            Query = "select count(*) from rekam_medis where no_kmr = '" + noRuangan + "'";
+
+            // Eksekusi query
+            int count = Convert.ToInt32(server.eksekusiQuery(Query).Rows[0][0]);
+
+            // Jika ada data di tabel medis dengan no ruangan yang sama
+            // Maka status kamar dianggap penuh
+            if (count > 0)
+                return false; // Kamar terisi
+            else
+                return true; // Kamar kosong
+        }
+
+
+
+
+    }
+}
